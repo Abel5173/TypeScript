bf51a1b998ddf76120d337848327f0d2637c860b
Abel <abelzeleke5173@gmail.com>
2023-03-15 08:49:47 +0300

    Figured out that you first need to transpile you .ts file to .js to run you code otherwise it will generate error

--- ToDoApp/index.js
+++ ToDoApp/index.js
@@ -1,8 +1,7 @@
-var c = document.getElementById("list");
-var container = document.getElementById("container");
-var button = document.getElementById("add");
-button.addEventListener('click', function () {
-    var paragraph = document.createElement('p');
-    paragraph.textContent = c.value;
-    container.appendChild(paragraph);
+let checkbox = document.querySelectorAll('input[type="checkbox"]');
+var x = 1;
+checkbox.forEach(element => {
+    let t = element.textContent;
+    console.log(x+ " " +t+"next");
+    x++;    
 });
--- ToDoApp/index.ts
+++ ToDoApp/index.ts
@@ -1,9 +1,2 @@
-var c = document.getElementById("list") as HTMLInputElement;
-var container = document.getElementById("container") as HTMLDivElement;
-var button = document.getElementById("add") as HTMLButtonElement;
-
-button.addEventListener('click', function(){
-let paragraph = document.createElement('p');
-paragraph.textContent = c.value;
-container.appendChild(paragraph);
-});
+// const paragraph = document.querySelector('p')!; // Use ! to assert that the element is not null
+// console.log(paragraph.textContent); // Safe to access properties of paragraph since TypeScript knows it's not null
