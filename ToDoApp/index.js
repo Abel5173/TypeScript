9b5aeee2ae0f97697f6e6d0f7dad6de09fce28a0
Abel <abelzeleke5173@gmail.com>
2023-03-15 09:36:57 +0300

    Figured out that you first need to transpile you .ts file to .js to run you code otherwise it will generate error

--- ToDoApp/index.js
+++
@@ -1,7 +0,0 @@
-let checkbox = document.querySelectorAll('input[type="checkbox"]');
-var x = 1;
-checkbox.forEach(element => {
-    let t = element.textContent;
-    console.log(x+ " " +t+"next");
-    x++;    
-});
